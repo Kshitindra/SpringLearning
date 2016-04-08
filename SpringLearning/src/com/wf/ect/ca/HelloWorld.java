166fc4a548daa4822d084ef141fed218df38743e
Kshitindra <mail4kjain@gmail.com>
2016-04-08 06:21:46 +0530

    Hello world

---
+++ SpringLearning/src/com/wf/ect/ca/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.wf.ect.ca;
+
+public class HelloWorld {
+	
+	private String message;
+
+	public void getMessage() {
+		System.out.println("Your Message "+message);
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+
+}
